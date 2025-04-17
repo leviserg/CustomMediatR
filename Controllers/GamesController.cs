1368ec8e35e9860d072854b16936d6496ea86e12
serhii.levytskyi-ext <serhii.levytskyi-ext@ldc.com>
2025-04-17 14:02:59 +0300

    added direct handler usage

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -7,13 +7,15 @@ namespace CustomMediatR.Controllers
 
     [ApiController]
     [Route("api/[controller]")]
-    public class GamesController(ISender sender) : ControllerBase
+    // public class GamesController(ISender sender) : ControllerBase
+    public class GamesController(IRequestHandler<GamesRequest, GamesResponse> handler) : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetGamesAsync([FromQuery]string searchText, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetGamesAsync([FromQuery]string? searchText, CancellationToken cancellationToken)
         {
             var request = new GamesRequest(searchText);
-            var games = await sender.SendAsync<GamesResponse>(request, cancellationToken);
+            //var games = await sender.SendAsync<GamesResponse>(request, cancellationToken);
+            var games = await handler.HandleAsync(request, cancellationToken);
             return Ok(games);
         }
     }
